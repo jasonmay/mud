ceb2a56a08b093e60cba10567946e7284a0981e1
Jason May <jason.a.may@gmail.com>
2009-07-14 20:29:55 -0400

    look at the front of the list instead of the back

--- lib/MUD/Server.pm
+++ lib/MUD/Server.pm
@@ -92,7 +92,7 @@ sub mud_client_input {
     my $player = $self->universe->players->{$wheel_id};
     $input =~ s/[\r\n]*$//;
     $_[HEAP]{client}{$wheel_id}->put(
-        $player->input_state->[-1]->run($player, $input)
+        $player->input_state->[0]->run($player, $input)
     );
 };
 
